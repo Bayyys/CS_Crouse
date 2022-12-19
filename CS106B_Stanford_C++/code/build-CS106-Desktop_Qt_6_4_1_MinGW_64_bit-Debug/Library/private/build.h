dd922626f08329bafcaaa6a5dc04a6ea9edd8075
BAY <475417309@qq.com>
2022-12-20 00:16:50 +0800

    Add files 'assign 1 \& assign 2' Bay-22.12.20

---
+++ CS106B_Stanford_C++/code/build-CS106-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2022.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Tue Dec 20 00:07:33 2022"
+#define SPL_BUILD_USER ""
+
+#endif
