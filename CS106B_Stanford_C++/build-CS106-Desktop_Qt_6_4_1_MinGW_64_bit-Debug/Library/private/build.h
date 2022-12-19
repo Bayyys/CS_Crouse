f932f9af37cd1464fb05b9faad878f2b3693fe4a
BAY <475417309@qq.com>
2022-12-19 23:31:42 +0800

    Add files 'Qt_CS106' Bay-22.12.19

---
+++ CS106B_Stanford_C++/build-CS106-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Mon Dec 19 23:26:28 2022"
+#define SPL_BUILD_USER ""
+
+#endif
