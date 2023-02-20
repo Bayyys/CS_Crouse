10f493120dd30a1c0eb6f25ba299dbd4f7add5c6
bayyy <475417309@qq.com>
2023-02-20 19:17:58 +0800

    add 'Android_test_chapter03' bayyy-23.02.20

---
+++ Android/Android_test/app/src/main/java/com/example/test/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
