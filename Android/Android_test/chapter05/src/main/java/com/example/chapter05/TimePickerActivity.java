fb871b648cea2378088dc383f559dc999fbafe5b
bayyy <475417309@qq.com>
2023-02-23 11:26:48 +0800

    stydy* "Android-chapter05" bayyy-23.02.23

---
+++ Android/Android_test/chapter05/src/main/java/com/example/chapter05/TimePickerActivity.java
@@ -0,0 +1,14 @@
+package com.example.chapter05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_picker);
+    }
+}
